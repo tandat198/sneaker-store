e51324b0a2d8bad1bf95379e51fab6f0bef513ff
tandat198 <tandat198@gmail.com>
2020-06-20 23:15:56 +0700

    add template

--- src/App.js
+++ src/App.js
@@ -1,15 +1,81 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.less";
+import { Card, Divider } from "antd";
+import FadeIn from "react-fade-in";
 
-function App() {
-    return (
-        <Router>
-            <Switch>
-                <Route />
-            </Switch>
-        </Router>
-    );
+const { Meta } = Card;
+
+class App extends React.Component {
+    state = { phones: [], tablets: [] };
+    componentDidMount() {
+        this.getPhones();
+        this.getTablets();
+    }
+
+    getPhones = async () => {
+        const res = await fetch(" http://localhost:3004/phones?_limit=4", {
+            method: "GET",
+            headers: { "Content-Type": "application/json" },
+        });
+        const phones = await res.json();
+        this.setState({ phones });
+    };
+
+    getTablets = async () => {
+        const res = await fetch(" http://localhost:3004/tablets?_limit=4", {
+            method: "GET",
+            headers: { "Content-Type": "application/json" },
+        });
+        const tablets = await res.json();
+        this.setState({ tablets });
+    };
+
+    render() {
+        return (
+            <>
+                <div className='header'>
+                    <div className='container header-container'>
+                        <div className='logo'>Mobile Store</div>
+                        <div className='right-nav'>
+                            <div>Smartphone</div>
+                            <div>Tablet</div>
+                        </div>
+                    </div>
+                </div>
+                <div className='container product-container'>
+                    <div className='phone-list'>
+                        <div className='list-title'>
+                            <span>Điện thoại nổi bật</span>
+                        </div>
+                        <div className='phones-container'>
+                            {this.state.phones.map(({ id, name, brand, thumbnail, price: p }) => (
+                                <FadeIn className='card'>
+                                    <Card key={id} hoverable cover={<img alt={name} src={thumbnail} />}>
+                                        <Meta title={`${brand} ${name}`} description={`${parseInt(p).toLocaleString()}`} />
+                                    </Card>
+                                </FadeIn>
+                            ))}
+                        </div>
+                    </div>
+                    <Divider dashed />
+                    <div className='tablet-list'>
+                        <div className='list-title'>
+                            <span>Tablet nổi bật</span>
+                        </div>
+                        <div className='tablets-container'>
+                            {this.state.tablets.map(({ id, name, brand, thumbnail, price: p }) => (
+                                <FadeIn className='card'>
+                                    <Card key={id} hoverable cover={<img alt={name} src={thumbnail} />}>
+                                        <Meta title={`${brand} ${name}`} description={`${parseInt(p).toLocaleString()}`} />
+                                    </Card>
+                                </FadeIn>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </>
+        );
+    }
 }
 
 export default App;
