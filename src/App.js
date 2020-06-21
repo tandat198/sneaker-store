626bf6f3726fe50d4f8c3ba8f05edc422cb8980f
tandat198 <tandat198@gmail.com>
2020-06-21 10:11:04 +0700

    add starter project

--- src/App.js
+++ src/App.js
@@ -1,52 +1,68 @@
 import React from "react";
 import "./App.less";
-import { Card, Divider } from "antd";
+import { Card, Divider, Breadcrumb, Button, InputNumber } from "antd";
 import FadeIn from "react-fade-in";
 
 const { Meta } = Card;
 
 class App extends React.Component {
-    state = { phones: [], tablets: [] };
-    componentDidMount() {
-        this.getPhones();
-        this.getTablets();
-    }
-
-    getPhones = async () => {
-        const res = await fetch(" http://localhost:3004/phones?_limit=4", {
-            method: "GET",
-            headers: { "Content-Type": "application/json" },
-        });
-        const phones = await res.json();
-        this.setState({ phones });
-    };
-
-    getTablets = async () => {
-        const res = await fetch(" http://localhost:3004/tablets?_limit=4", {
-            method: "GET",
-            headers: { "Content-Type": "application/json" },
-        });
-        const tablets = await res.json();
-        this.setState({ tablets });
-    };
+    state = { phones: [], tablets: [], phone: {}, tablet: {} };
 
     render() {
         return (
             <>
                 <div className='header'>
                     <div className='container header-container'>
-                        <div className='logo'>Mobile Store</div>
+                        <div className='logo'>
+                            <a href='/'>Mobile Store</a>
+                        </div>
                         <div className='right-nav'>
-                            <div>Smartphone</div>
-                            <div>Tablet</div>
+                            <a href='/phones'>Smartphone</a>
+                            <a href='/tablets'>Tablet</a>
                         </div>
                     </div>
                 </div>
+                {/* Home Page */}
                 <div className='container product-container'>
                     <div className='phone-list'>
                         <div className='list-title'>
                             <span>Điện thoại nổi bật</span>
                         </div>
+                        <div className='phones-container'>
+                            {/* <FadeIn className='card'>
+                                    <Card key={id} hoverable cover={<img alt={name} src={thumbnail} />}>
+                                        <Meta title={`${brand} ${name}`} description={`${parseInt(p).toLocaleString()}`} />
+                                    </Card>
+                                </FadeIn> */}
+                        </div>
+                    </div>
+                    <Divider dashed />
+                    <div className='tablet-list'>
+                        <div className='list-title'>
+                            <span>Tablet nổi bật</span>
+                        </div>
+                        <div className='tablets-container'>
+                            {/* <FadeIn className='card'>
+                                    <Card key={id} hoverable cover={<img alt={name} src={thumbnail} />}>
+                                        <Meta title={`${brand} ${name}`} description={`${parseInt(p).toLocaleString()}`} />
+                                    </Card>
+                                </FadeIn> */}
+                        </div>
+                    </div>
+                </div>
+
+                {/* Phone Page */}
+                {/* <div className='all-phones'>
+                    <div className='container all-phones-container'>
+                        <Breadcrumb>
+                            <Breadcrumb.Item>
+                                <a href='/'>Home</a>
+                            </Breadcrumb.Item>
+                            <Breadcrumb.Item>
+                                <a href='/phones'>Phones</a>
+                            </Breadcrumb.Item>
+                        </Breadcrumb>
+                        <Divider />
                         <div className='phones-container'>
                             {this.state.phones.map(({ id, name, brand, thumbnail, price: p }) => (
                                 <FadeIn className='card'>
@@ -57,11 +73,20 @@ class App extends React.Component {
                             ))}
                         </div>
                     </div>
-                    <Divider dashed />
-                    <div className='tablet-list'>
-                        <div className='list-title'>
-                            <span>Tablet nổi bật</span>
-                        </div>
+                </div>
+
+                {/* Tablets Page */}
+                {/* <div className='all-tablets'>
+                    <div className='container all-tablets-container'>
+                        <Breadcrumb>
+                            <Breadcrumb.Item>
+                                <a href='/'>Home</a>
+                            </Breadcrumb.Item>
+                            <Breadcrumb.Item>
+                                <a href='/tablets'>Tablets</a>
+                            </Breadcrumb.Item>
+                        </Breadcrumb>
+                        <Divider />
                         <div className='tablets-container'>
                             {this.state.tablets.map(({ id, name, brand, thumbnail, price: p }) => (
                                 <FadeIn className='card'>
@@ -72,7 +97,49 @@ class App extends React.Component {
                             ))}
                         </div>
                     </div>
-                </div>
+                </div> */}
+
+                {/* Product Info */}
+                {/* <div className='product-item'>
+                    <div className='container product-container'>
+                        <Breadcrumb>
+                            <Breadcrumb.Item>
+                                <a href='/'>Home</a>
+                            </Breadcrumb.Item>
+                            <Breadcrumb.Item>
+                                <a href='/phones'>Phones</a>
+                            </Breadcrumb.Item>
+                            <Breadcrumb.Item>
+                                <a href='/'>{phone.name}</a>
+                            </Breadcrumb.Item>
+                        </Breadcrumb>
+                        <Divider />
+                        <div className='info'>
+                            <div className='basic-info'>
+                                <div className='product-img'>
+                                    <img src={phone.imageUrl} alt={phone.name} />
+                                </div>
+                                <div className='basic-info-wp'>
+                                    <span>{`${phone.brand} ${phone.name}`}</span>
+                                    <span>{parseInt(phone.price).toLocaleString()}</span>
+                                    <div className='add-to-cart'>
+                                        <div className='quantity'>
+                                            <InputNumber min={1} max={10} defaultValue={1} />
+                                        </div>
+                                        <Button type='primary'>Add To Cart</Button>
+                                    </div>
+                                </div>
+                            </div>
+                            <ul className='detail-info'>
+                                <li>Storage: {phone.storage} GB</li>
+                                <li>Memory: {phone.memory} GB</li>
+                                <li>Screen Size: {phone.screenSize} inches</li>
+                                <li>Chipset: {phone.chipset}</li>
+                                <li>OS: {phone.os}</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div> */}
             </>
         );
     }
